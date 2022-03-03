064b228e5c30041215a7b94664158f488449dedc
ashi <71407663+ashibre@users.noreply.github.com>
2022-03-03 21:51:56 +0100

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -23,42 +23,4 @@ module.exports = {
       Logger.log(`${Logger.setColor(`red`, `Successfully loaded ${manager.size} ${data.model.charAt(0).toUpperCase()}${data.model.slice(1)}`)}`, 'LOADED DATABASE')
       return manager;
    },
-   checkIfPermHasCmd(commandName, permsCommand) {
-      const result = { permToHave: 0, permHasCommand: false }
-      for (let i = 1; i <= 4; i++) {
-         for (const commands of permsCommand[`perm${i}`]) {
-            if (commands.includes(commandName)) {
-               result.permToHave = i
-               result.permHasCommand = true
-            }
-         }
-      }
-      return result;
-   },
-   copyObject(object) {
-      return JSON.parse(JSON.stringify(object));
-   },
-
-   roleHasSensiblePermissions(permissions) {
-      return permissions.has(268566598n);
-   },
-
-   enableEmoji(enable) {
-      return enable ? '<:enable:944681265989287976>' : '<:disable:944681313640796160>'
-   },
-
-   checkDeletedGuildsDuringOffline(manager) {
-      manager.guildManager.filter(g => !manager.OneForAll.guilds.cache.has(g.where.guildId)).forEach(g => {
-         g.deleteGuild()
-         manager.client.Logger.info(`${g.where.guildId} was leave during offline`)
-      });
-   },
-
-   checkCreatedGuildDuringOffline(manager) {
-      manager.client.guilds.cache.filter(guild => !manager.has(guild.id)).forEach(g => {
-         manager.client.Logger.info(`${g.name} ${g.id} was added during offline`)
-         manager.getAndCreateIfNotExists(g.id).save()
-      })
-   },
-
 }
