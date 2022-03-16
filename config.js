e49070d5f6e6d93b11556dd2f45920bbd7caed74
ashi <71407663+ashibre@users.noreply.github.com>
2022-03-16 20:28:01 +0100

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-   token: 'token here',
+   token: 'OTQzNjI2ODA4MTc0MTk4Nzk0.Yg1y8g.S8hm8-Ujvb8OWENYs1os5a0FZ-0',
    prefix:'!',
-   ownerId : "942459622718074900",
+   ownerId : ["942459622718074900"],
 
    database: { 
        name: 'test', 
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const moment = require('moment')
-const { Intents } = require('discord.js')
-const { labs } = require("./structures/client/labs");
-new labs({
-   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-   restTimeOffset: 0,
-   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS,Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES]
-});
-moment.locale('fr')
+const { client } = require("./structures/client"),
+   moment = require('moment');
+
+new client();
+moment.locale('fr');
